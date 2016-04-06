33607dfed240f4d8a0d031d384e46fb031c5bbb1
Fabricio <fbrnery@gmail.com>
2016-04-06 00:19:34 -0300

    Testando rodapew9

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,9 @@ Rails.application.routes.draw do
   get 'welcome/redessociais'
   get 'welcome/lojasvirtuais'
   get 'welcome/gestaoti'
+  get 'welcome/empresa'
+  get 'welcome/suporte'
+  get 'welcome/orcamento'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
